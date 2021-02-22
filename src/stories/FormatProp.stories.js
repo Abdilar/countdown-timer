dc9e51e7efcbeab8ce7517841752c4eac2eea7eb
saeedabdilar <saeed.abdilar@gmail.com>
2021-02-22 04:14:17 +0330

    [Feature] new them added / new option added to format prop (in stories)

--- src/stories/FormatProp.stories.js
+++ src/stories/FormatProp.stories.js
@@ -12,8 +12,10 @@ const customClassName = {
 
 export const doubleDigit = () => <CountdownTimer time={3770} format="HH:MM:SS" />;
 export const singleDigit = () => <CountdownTimer time={3770} format="hh:mm:ss" />;
+export const position = () => <CountdownTimer time={3770} format="SS:MM:HH" />;
 export const customFormat = () => <CountdownTimer className={customClassName} time={3770} format="HHh MMm SSs"/>;
 
 doubleDigit.storyName = "Leading zero for single-digit";
 singleDigit.storyName = "No leading zero for single-digit";
+position.storyName = "Swapped hour(s) and seconds";
 customFormat.storyName = "Custom format";
